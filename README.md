9c337287f27d361edc710439f89c03bb0b48da83
Tajwar <naharsoftbd@gmail.com>
2025-07-24 21:42:53 +0600

    Added readme file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# laravel-12-react-CRUD
+# Laravel Starter Kit
+- Laravel v12.x
+- PHP v8.4.x
+- MySQL v8.1.x (default)
+- MariaDB v10.11.x
+- PostgreSQL v16.x
+- pgAdmin v4.x
+- phpMyAdmin v5.x
+- Mailpit v1.x
+- Node.js v18.x
+- NPM v10.x
+- Yarn v1.x
+- Vite v5.x
+- Rector v1.x
+- Redis v7.2.x
+
+# How To Deploy
+
+### For first time only !
+- `git clone https://github.com/naharsoftbd/laravel-12-react-CRUD.git`
+- `cd laravel-12-react-CRUD`
+- `composer install`
+- `cp .env.example .env`
+- `php artisan key:generate`
+- `Configuring the database in .env`
+- `php artisan migrate`
+- `php artisan db:seed --seeder=PermissionSeeder`
+- `npm install`
+- `npm run dev or npm run build`
+- `php artisan serve`
+
+
+
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Support\Facades\Hash;
 
 class PermissionSeeder extends Seeder
 {
@@ -23,5 +26,19 @@ class PermissionSeeder extends Seeder
         foreach($permissions as $key => $value){
             Permission::create(['name' => $value]);
         }
+
+      $role = Role::create(['name' => 'Admin']);
+      $role->givePermissionTo('View');
+      $role->givePermissionTo('Create');
+      $role->givePermissionTo('Edit');
+      $role->givePermissionTo('Delete');
+
+      
+      $user = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'abujafarmdsalah@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole($role);
     }
 }
